27cff55fd44ec5c096fc1aee58bea7ae2967dc9e
upcoderWX <2559013807@qq.com>
2021-01-08 09:43:38 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
   <div id="app">
-    456
+    123
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'assets/css/base.css';
 </style>
